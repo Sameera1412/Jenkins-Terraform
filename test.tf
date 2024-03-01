3583167c6e6d22e2e1cb1c86e3f4be110c13c915
Sameera1412 <132312113+Sameera1412@users.noreply.github.com>
2024-03-01 10:46:19 +0530

    Updated ami-id per region

--- test.tf
+++ test.tf
@@ -17,7 +17,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "myec2" {
-   ami = "ami-09988af04120b3591"
+   ami = "ami-0f5daaa3a7fb3378b"
    instance_type = "t2.micro"
    key_name = "DevOps Keypair"
    tags = {
